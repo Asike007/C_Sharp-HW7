41028c76f357bd9d7c408462b1cc663884bea226
Aslanbek <miz_asl_93@mail.ru>
2022-12-21 23:19:33 +0600

    HW #7 completed

---
+++ task50/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 50: Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.WriteLine("Введите количетсво строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите количетсво столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите минимальное число: ");
+int min = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите максимальное число: ");
+int max = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите стороку искомого элемента: ");
+int searchingRow = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите столбец искомого элемента: ");
+int searchingColumn = int.Parse(Console.ReadLine()!);
+
+int[,] array = NewArray(rows, columns, min, max);
+PrintArray(array);
+Searching(array);
+
+int[,] NewArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Searching(int[,] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == searchingRow && j == searchingColumn)
+            {
+                res = arr[searchingRow, searchingColumn];
+                Console.Write($"Искоемое значение равно: {res} ");
+                return;
+            }
+        }
+    }
+    Console.Write($"Такого числа в массиве нет");
+}
---
+++ task52/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите количство строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+int[,] array = NewArray(rows, columns, 1, 10);
+
+PrintArray(array);
+Console.WriteLine();
+
+Console.Write("Среднее арифметическое каждого столбца: ");
+ArithmeticMean(array);
+
+int[,] NewArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticMean(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double res = 1;
+        int a = columns;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            res = res * arr[i, j];
+        }
+        res = res / a;
+        Console.Write($"{Math.Round(res, 1)}; ");
+    }
+}
